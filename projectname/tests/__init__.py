82b2279bc8ba3f0dd04efcf7ecce8b6e473f2d6d
Casokaks <casokaks@gmail.com>
2021-08-23 00:03:47 +0000

    Change config

--- projectname/tests/__init__.py
+++ projectname/tests/__init__.py
@@ -6,9 +6,3 @@ Author: Casokaks (https://github.com/Casokaks/)
 Created on: Aug 15th 2021
 
 """
-
-# -----------------------------------------------------------------------------------
-# Imports from modules
-# -----------------------------------------------------------------------------------
-from .test_config import *
-from .test import *
--- projectname/tests/test.py
+++ projectname/tests/test.py
@@ -8,13 +8,11 @@ Created on: Aug 15th 2021
 """
 
 from unittest import TestCase
-from .test_config import TEST_CONFIG
-# import package to test
 
 class TestSomething(TestCase):
     """Test Class."""
     
-    def get_test_config(self, t=TEST_CONFIG):
+    def test(self):
         """Empty test."""
-        return t
+        pass
     
